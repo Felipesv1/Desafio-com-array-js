df45f8b1866b5e6c79c70b31fb74fb5b66eff950
Felipe Pereira <104787971+Felipesv1@users.noreply.github.com>
2022-06-22 21:54:30 -0300

    Desafio concluido

---
+++ script.js
@@ -0,0 +1,20 @@
+// Crie um array que receba 5 itens e exiba no console.
+    let itens = ["caderno","lapis","borracha","caneta","apontador"];
+    console.log(itens)
+ 
+// Utilize um método para adicionar um nome ao inicio do array.
+    itens.unshift("estojo");
+
+// Utilize um método para remover o último nome do array.
+    itens.pop();
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+    itens.push("pedro","joaquim");
+
+// Utilize um método para remover o primeiro nome do array.
+    itens.shift();
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+ let numbers = [7,5,6,3,8,9,2,1,4]
+ numbers.sort();
+ console.log(numbers)
